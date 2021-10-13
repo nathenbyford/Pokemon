1548e0bb9a0e9eb308d56ded2548664434eae954
nathenbyford <Nathenbyford@gmail.com>
2021-10-13 16:45:26 -0500

    Begin report

--- Scripts/Byford_EDA.r
+++ Scripts/Byford_EDA.r
@@ -32,7 +32,10 @@ ggplot(pokemon, aes(typing, speed)) +
 
 # slpit type into primary and secondary
 
-pokemon_new <- pokemon %>% separate(typing, into = c("primary", "secondary"))
+pokemon_new <-  pokemon %>%
+  separate(typing, into = c("primary", "secondary")) %>%
+  mutate(BMI = (weight/10)/((height/10)^2)) %>%
+  filter(BMI <= 50000)
 
 unique(pokemon_new$primary)
 unique(pokemon_new$secondary)
@@ -57,7 +60,8 @@ pokemon_new |>
 
 ## Looking at Base happyness
 
-  # Potential points of interest; Type, baby_pokemon, 
+  # Potential points of interest; Type, baby_pokemon, legendary, primary_color,
+  #mythical, BMI, shape, female rate
 
 pokemon_new |> ggplot(aes(primary, base_happiness)) +
   geom_jitter(alpha = 0.2) 
@@ -65,5 +69,51 @@ pokemon_new |> ggplot(aes(primary, base_happiness)) +
 pokemon_new |> ggplot(aes(primary, base_happiness)) +
   geom_bin2d()
 
+pokemon_new |> ggplot(aes(baby_pokemon, base_happiness)) +
+  geom_jitter()
 
+pokemon_new |> ggplot(aes(baby_pokemon, base_happiness)) +
+  geom_violin()
 
+pokemon_new |> ggplot(aes(legendary, base_happiness)) +
+  geom_jitter()
+
+pokemon_new |> ggplot(aes(legendary, base_happiness)) +
+  geom_violin(aes(fill = legendary)) +
+  geom_boxplot(width = .03)
+
+pokemon_new |> ggplot(aes(primary_color, base_happiness)) +
+  geom_jitter()
+
+pokemon_new |> ggplot(aes(primary_color, base_happiness)) +
+  geom_boxplot()
+
+pokemon_new |> ggplot(aes(mythical, base_happiness)) +
+  geom_jitter()
+
+pokemon_new |> ggplot(aes(mythical, base_happiness)) +
+  geom_boxplot()
+
+pokemon_new |> ggplot(aes(BMI, base_happiness)) +
+  geom_jitter()
+
+pokemon_new |> ggplot(aes(shape, base_happiness)) +
+  geom_jitter(alpha = .1)
+
+pokemon_new |> ggplot(aes(shape, base_happiness)) +
+  geom_boxplot()
+
+pokemon_new |> ggplot(aes(female_rate, base_happiness)) +
+  geom_jitter(alpha = .2)
+
+pokemon_new |> ggplot(aes(as.factor(gen_introduced), base_happiness)) +
+  geom_jitter()
+
+pokemon_new |> ggplot(aes(as.factor(gen_introduced), base_happiness)) +
+  geom_boxplot()
+
+pokemon_new |> ggplot(aes(genderless, base_happiness)) +
+  geom_jitter()
+
+pokemon_new |> ggplot(aes(genderless, base_happiness)) +
+  geom_boxplot()
